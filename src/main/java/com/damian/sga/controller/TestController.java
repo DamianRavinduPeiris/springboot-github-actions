64d6a1a26875f2b3740b72cef2b2d5ebd3c4c18d
DamianRavinduPeiris <drpeiris3@gmail.com>
2024-09-03 15:13:41 +0530

    Edited the build .yml file to pull the Dockerimage from the ECR and deploy it to the EC2.

--- src/main/java/com/damian/sga/controller/TestController.java
+++ src/main/java/com/damian/sga/controller/TestController.java
@@ -3,14 +3,20 @@ package com.damian.sga.controller;
 import com.damian.sga.response.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("v1/test")
 public class TestController {
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public ResponseEntity<Response> hello() {
         return new ResponseEntity<>(new Response(200, "Hello World!", null), HttpStatus.OK);
     }
+    @GetMapping("/yo")
+    public ResponseEntity<Response> yoo() {
+        return new ResponseEntity<>(new Response(200, "yoooo!", null), HttpStatus.OK);
+    }
+
 }
